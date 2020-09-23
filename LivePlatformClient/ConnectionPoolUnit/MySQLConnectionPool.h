f29eccac2c9e26ebc78835e9289353b7a1d5cd12
THAT BOY <zhu.thatboy@outlook.com>
2020-09-23 09:48:38 +0800

    添加MySQL连接池

---
+++ LivePlatformClient/ConnectionPoolUnit/MySQLConnectionPool.h
@@ -0,0 +1,85 @@
+﻿#ifndef _MYSQL_CONNECTION_POOL_H_
+#define _MYSQL_CONNECTION_POOL_H_
+
+#include <mysql++/mysql++.h>
+#include <string>
+
+namespace thatboy
+{
+	namespace sql
+	{
+		class ConnectionPool : public mysqlpp::ConnectionPool
+		{
+		public:
+			ConnectionPool(std::string dbname
+				, std::string serverip
+				, std::string user
+				, std::string passwd
+				, int port = 3306
+				, std::string charset = std::string("utf8"))
+				: m_dbname(dbname)
+				, m_server_ip(serverip)
+				, m_user(user)
+				, m_password(passwd)
+				, m_charset(charset)
+				, m_port(port)
+			{
+				m_max_idle_time = 300;
+			}
+
+			virtual ~ConnectionPool()
+			{
+				clear();
+			}
+
+			const std::string& getDBName() const { return m_dbname; }
+			const std::string& getServerIP() const { return m_server_ip; }
+			const std::string& getUser() const { return m_user; }
+			const std::string& getPassword() const { return m_password; }
+			const std::string& getCharset() const { return m_charset; }
+			int getPort() const { return m_port; }
+
+			void setMaxIdleTime(int max_idle)
+			{
+				m_max_idle_time = max_idle;
+			}
+		protected:
+			virtual mysqlpp::Connection* create()
+			{
+				mysqlpp::Connection* conn = new mysqlpp::Connection(true);
+				mysqlpp::SetCharsetNameOption* pOpt = new  mysqlpp::SetCharsetNameOption(m_charset.c_str());
+				conn->set_option(pOpt);
+
+				conn->connect(m_dbname.empty() ? 0 : m_dbname.c_str(),
+					m_server_ip.empty() ? 0 : m_server_ip.c_str(),
+					m_user.empty() ? 0 : m_user.c_str(),
+					m_password.empty() ? "" : m_password.c_str(),
+					m_port);
+
+				return conn;
+			}
+
+			virtual void destroy(mysqlpp::Connection* cp)
+			{
+				delete cp;
+			}
+
+			virtual unsigned int max_idle_time()
+			{
+				return m_max_idle_time;
+			}
+
+		private:
+			std::string m_dbname;
+			std::string m_server_ip;
+			std::string m_user;
+			std::string m_password;
+			std::string m_charset;
+			int         m_port;
+			int         m_max_idle_time;
+		};
+		using mysqlpp::ScopedConnection;
+		using mysqlpp::Query;
+	}
+}
+#endif
---
+++ LivePlatformClient/ConnectionPoolUnit/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#include "MySQLConnectionPool.h"
+std::string db_name = "LiveUser";
+std::string db_ip = "thatboy.info";
+std::string db_user = "LiveClient";
+std::string db_passwd = "liveclient%007";
+int db_port = 3306;
+std::string db_charset = "utf8";
+
+int main()
+{
+	thatboy::sql::ConnectionPool db_pool(db_name, db_ip, db_user, db_passwd, db_port, db_charset);
+
+	try {
+		thatboy::sql::ScopedConnection conn(db_pool);
+		auto query = conn->query("select * from LiveUser.live_user");
+
+		auto ret = query.store();
+
+		for (auto it : ret)
+		{
+			for (auto it : it)
+			{
+				std::cout << it << std::endl;
+			}
+			std::cout << std::endl;
+		}
+	}
+	catch (const mysqlpp::Exception& e) {
+		std::cout << "error:" << e.what() << "\n";
+		return false;
+	}
+	Sleep(2000);
+	return 0;
+}
